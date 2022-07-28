b55332e20635082252ef3c6bbbc769a545b11b92
Jonathon Baxendell <j.bax626@gmail.com>
2022-07-28 18:45:07 -0500

    added input fields and ids

--- server/server.js
+++ server/server.js
@@ -4,7 +4,7 @@ const app = express();
 const PORT = 5000;
 
 // This must be added before GET & POST routes.
-app.use(bodyParser.urlencoded({extended:true}))
+app.use(bodyParser.urlencoded({extended:true}));
 
 // Serve up static files (HTML, CSS, Client JS)
 app.use(express.static('server/public'));
