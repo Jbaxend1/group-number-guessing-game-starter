a9ffd999d0c4ae0dcee4ab8bedd9985cac242728
Jonathon Baxendell <j.bax626@gmail.com>
2022-07-28 19:25:44 -0500

    get request guesses

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -2,4 +2,31 @@ $(document).ready(handleReady);
 
 function handleReady() {
   console.log("jquery is loaded!")
+
+  // $('#submit-guess').on('click',);
+  getGuesses();
+}
+
+function getGuesses(){
+  console.log('in getGuesses');
+  
+  $.ajax({
+    type: 'GET',
+    url: '/guesses'
+
+  }).then(function (response){
+    console.log(response);
+    $('#total-guesses').empty();
+    for(let i = 0; i < response.length; i++){
+      let guess = response[i];
+      $('#total-guesses').append(`
+        <ul>
+          <li>${guess.p1}</li>
+          <li>${guess.p2}</li>
+          <li>${guess.p3}</li>
+          <li>${guess.p4}</li>
+        </ul>
+      `)
+    }
+  })
 }
--- server/server.js
+++ server/server.js
@@ -9,8 +9,29 @@ app.use(bodyParser.urlencoded({extended:true}));
 // Serve up static files (HTML, CSS, Client JS)
 app.use(express.static('server/public'));
 
-// GET & POST Routes go here
+const guessArray = [
+  {
+    p1: 12,
+    p2: 7,
+    p3: 16,
+    p4: 22
+  }
+]
 
+function generateRandomNumber() {
+  return 8; // TODO: random number
+}
+// runs one time when the server starts
+let randomNumber = generateRandomNumber();
+app.get('/reset', (req, res) => {
+  randomNumber = generateRandomNumber();
+  res.send(200);
+});
+// GET & POST Routes go here
+// GET to return list of guesses
+app.get('/guesses', (req, res) =>{
+  res.send(guessArray);
+})
 
 app.listen(PORT, () => {
   console.log ('Server is running on port', PORT)
